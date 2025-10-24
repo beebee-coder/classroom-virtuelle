ec538c111a587c90e5316c532d3515f0c37061bc
ahmed abbes <ahmedyoussefabbes001@gmail.com>
2025-10-24 19:31:29 +0000

    Try fixing this error: `Server Error: Error: Cannot find module './vendo

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // serverActions est maintenant activé par défaut, la clé peut être supprimée.
-  },
   // Ajout pour autoriser les requêtes depuis l'environnement de développement cloud
   allowedDevOrigins: [
     "https://3000-firebase-studio-1761127865714.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev",
     "https://6000-firebase-studio-1761127865714.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev",
   ],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
