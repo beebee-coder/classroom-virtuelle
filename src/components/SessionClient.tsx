9d6d990929d43dd9dd310d75ee2709fb05a09399
ahmed abbes <ahmedyoussefabbes001@gmail.com>
2025-10-25 14:22:24 +0000

    il y'a un espace vide a gauche et un espace vide a droite dans la page s

--- src/components/SessionClient.tsx
+++ src/components/SessionClient.tsx
@@ -481,7 +481,7 @@ export default function SessionClient({
         activeTool={activeTool}
         onToolChange={handleToolChange}
       />
-      <main className="flex-1 flex flex-col container mx-auto px-4 sm:px-6 lg:px-8 min-h-0">
+      <main className="flex-1 flex flex-col px-4 sm:px-6 lg:px-8 min-h-0">
         <PermissionPrompt />
         {currentUserRole === 'PROFESSEUR' ? (
           <TeacherSessionView
