7ab260d627da7b0fcffd378542b22f7d0422efac
ahmed abbes <ahmedyoussefabbes001@gmail.com>
2025-10-30 23:03:20 +0000

    studio-4170183741:~/studio{main}$ npm run dev
    > classroom-connector@0.1

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com', port: '', pathname: '/**' },
       { protocol: 'https', hostname: 'picsum.photos', port: '', pathname: '/**' },
       { protocol: 'https', hostname: 'api.dicebear.com', port: '', pathname: '/**' },
-      { protocol: 'https://res.cloudinary.com', port: '', pathname: '/**' }
+      { protocol: 'https', hostname: 'res.cloudinary.com', port: '', pathname: '/**' }
     ],
   },
 };
