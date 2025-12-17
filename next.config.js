1c527d7e4cba75f5bbbc30650e5366796a4156da
ahmed abbes <ahmedyoussefabbes001@gmail.com>
2025-12-17 16:17:04 +0000

    ],   page: '/session/cmja7hutf0008u2zjoi8hpbe4' }  GET /session/cmja7hut

--- next.config.js
+++ next.config.js
@@ -12,28 +12,6 @@ const nextConfig = {
       }
     ],
   },
-  async headers() {
-    return [
-      {
-        // Appliquer ces en-têtes à toutes les routes de l'application
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Autoriser n'importe quelle origine (sécuritaire pour le dev)
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, OPTIONS, PATCH, DELETE, POST, PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
